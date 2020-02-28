c1befad92c205078d157e52e368cb04d4986106c
William Ruberte <wruberte@incomm.com>
2020-02-28 13:39:10 -0500

    Initial Commit of Spring Initializer Generated Files

---
+++ src/main/java/com/ruberte/example/restapi/RuberteRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ruberte.example.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuberteRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuberteRestapiApplication.class, args);
+	}
+
+}
